016f7e178c9aee2469322fa72dfebeb2516b9bd3
James Campbell <jaymznis@gmail.com>
2021-06-23 11:40:05 -0400

    note index view created

--- ElevenNote.Data/Note.cs
+++ ElevenNote.Data/Note.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ElevenNote.Data
 {
-    class Note
+    public class Note
     {
+        [Key]
+        public int NoteId { get; set; }
+
+
+        //[ForeignKey(nameof(Category))]
+        //public int? CategoryId { get; set; }
+        //public virtual Category Category { get; set; }
+
+
+        [Required]
+        public Guid OwnerId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public DateTimeOffset? ModifiedUtc { get; set; }
     }
 }
